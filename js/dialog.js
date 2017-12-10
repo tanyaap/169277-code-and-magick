2669e950bd46aad2ffe8504ec9f2cefafa890ed4
tanyaap <aptat@inbox.ru>
2017-12-10 17:39:02 +0700

    Учебный проект: в движении

--- js/dialog.js
+++ js/dialog.js
@@ -71,4 +71,36 @@
     fireballColors.style.backgroundColor = window.util.getRandomElement(FIREBALLS);
   }
   fireballColors.addEventListener('click', onClickFireball);
+
+  var dialogHandler = setupWizard.querySelector('.setup-user-pic');
+  dialogHandler.style.zIndex = 1; // только как грузить аватарку, если будет в заданиях?
+
+  function onHandlerDrag(evt) {
+    evt.preventDefault();
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+      setupWizard.style.top = (setupWizard.offsetTop - shift.y) + 'px';
+      setupWizard.style.left = (setupWizard.offsetLeft - shift.x) + 'px';
+    };
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  }
+  dialogHandler.addEventListener('mousedown', onHandlerDrag);
 })();
