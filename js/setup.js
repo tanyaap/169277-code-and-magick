46b2450adc7bad1446853f6788b73f4ec1a1d3f1
tanyaap <aptat@inbox.ru>
2017-12-19 18:09:36 +0700

    Учебный проект: pimp my mage. Исправление2 19.12.17

--- js/setup.js
+++ js/setup.js
@@ -1,34 +1,16 @@
 'use strict';
 
 (function () {
-  /*  var WIZARDS_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  var WIZARDS_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-  var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-  var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];*/
-
   var setupWizard = document.querySelector('.setup');
 
   var similarWizardsList = setupWizard.querySelector('.setup-similar-list');
   var wizardTemplate = document.querySelector('#similar-wizard-template').content;
 
-  /*  var wizardsSet = [
-    {'name': '', 'coatColor': '', 'eyesColor': ''},
-    {'name': '', 'coatColor': '', 'eyesColor': ''},
-    {'name': '', 'coatColor': '', 'eyesColor': ''},
-    {'name': '', 'coatColor': '', 'eyesColor': ''}
-  ];
-
-  for (var i = 0; i < wizardsSet.length; i++) {
-    wizardsSet[i].name = window.util.getRandomElement(WIZARDS_NAMES) + ' ' + window.util.getRandomElement(WIZARDS_LAST_NAMES);
-    wizardsSet[i].coatColor = window.util.getRandomElement(COAT_COLORS);
-    wizardsSet[i].eyesColor = window.util.getRandomElement(EYES_COLORS);
-  }*/
-
   function renderWizard(wizardArr) {
     var wizardFromSet = wizardTemplate.cloneNode(true);
     wizardFromSet.querySelector('.setup-similar-label').textContent = wizardArr.name;
-    wizardFromSet.querySelector('.wizard-coat').style.fill = wizardArr.coatColor;
-    wizardFromSet.querySelector('.wizard-eyes').style.fill = wizardArr.eyesColor;
+    wizardFromSet.querySelector('.wizard-coat').style.fill = wizardArr.colorCoat;
+    wizardFromSet.querySelector('.wizard-eyes').style.fill = wizardArr.colorEyes;
     return wizardFromSet;
   }
 
@@ -43,7 +25,12 @@
 
   function errorHandler(errorMessage) {
     var node = document.createElement('div');
-    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: #dd1f1f; border: 2px solid #fff; font-weight: bold';
+    node.style.zIndex = 100;
+    node.style.margin = '0 auto';
+    node.style.textAlign = 'center';
+    node.style.backgroundColor = '#dd1f1f';
+    node.style.border = '2px solid #fff';
+    node.style.border = 'bold';
     node.style.position = 'absolute';
     node.style.left = 0;
     node.style.right = 0;
